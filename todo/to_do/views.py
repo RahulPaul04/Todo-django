a9fb088369ab24b739af5944456fdce01cd1565c
rahul <rahulpaul0498@gmail.com>
2023-05-16 20:03:58 +0530

    items can now be added,saved to database,then displayed

--- todo/to_do/views.py
+++ todo/to_do/views.py
@@ -4,5 +4,13 @@ from . models import items
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        if name:
+            it = items(name=name)
+            try:
+                it.save()
+            except Exception as e:
+                print(f"Error saving item: {e}")
     item = items.objects.all()
     return render(request,"index.html",{"items":item})
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from to_do import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('',views.index,name="index"),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
